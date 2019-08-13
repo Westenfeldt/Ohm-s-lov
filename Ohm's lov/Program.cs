1286b4d38cb7c438019dbe3138df82e461ec2afe
Stefan Vestenfeldt <stefanwestenfeldt@gmail.com>
2019-08-13 13:24:26 +0200

    Add project files.

---
+++ Ohm's lov/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Ohm_s_lov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // modstang * strøm = spænd --- effekt = strøm * spænding
+            string modstand, strøm;
+            double spænding, effekt; 
+            Console.Write("Du er ved at udregne spænding i Ohm's lov. ");
+
+
+            Console.WriteLine("Hvad er modstanden?");
+            modstand = Console.ReadLine();
+
+            double modstand2 = float.Parse(modstand);
+
+            Console.WriteLine("Hvad er strømstyrken?");
+            strøm = Console.ReadLine();
+
+            double strøm2 = float.Parse(strøm);
+
+
+
+            // udregning
+            spænding = modstand2 * strøm2;
+            effekt = strøm2 * spænding;
+
+            Console.WriteLine("Spændingen er {0}", spænding);
+            Console.WriteLine("Effekten er {0}", effekt);
+
+            
+
+
+
+
+
+        }
+    }
+}
